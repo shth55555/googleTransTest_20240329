96f9fd049643d1dd2ceb8e8d6936a8fa15da3d9e
shth55555 <shth5555@naver.com>
2024-03-29 16:16:51 +0900

    while문

--- googleTransTest.py
+++ googleTransTest.py
@@ -1,15 +1,34 @@
 import googletrans
+#
+# trans = googletrans.Translator()  #구글 변역 객체 생성
+# result1 = trans.translate("뭐를 해볼까요? 중국어?", dest="en")
+# # print(googletrans.LANGUAGES) ->번역 언어의 dest에 삽입할 약자 찾기
+#
+# print(result1.text)
+#
+# result2 = trans.translate("뭐를 해볼까요? 중국어?", dest="ja")
+#
+# result3 = trans.translate("뭐를 해볼까요? 중국어?", dest="zh-cn")
+#
+# print(result1.text)
+# print(result2.text)
+# print(result3.text)
 
-trans = googletrans.Translator()  #구글 변역 객체 생성
-result1 = trans.translate("뭐를 해볼까요? 중국어?", dest="en")
-# print(googletrans.LANGUAGES) ->번역 언어의 dest에 삽입할 약자 찾기
 
-print(result1.text)
+while True:
+    korStr = input("번역할 문장을 입력하세요:")
+    if korStr =="끝":
+        break
+    trans = googletrans.Translator()  #구글 변역 객체 생성
+    result1 = trans.translate(korStr, dest="en")
+    # print(googletrans.LANGUAGES) ->번역 언어의 dest에 삽입할 약자 찾기
 
-result2 = trans.translate("뭐를 해볼까요? 중국어?", dest="ja")
+    # print(result1.text)
 
-result3 = trans.translate("뭐를 해볼까요? 중국어?", dest="zh-cn")
+    result2 = trans.translate(korStr, dest="ja")
 
-print(result1.text)
-print(result2.text)
-print(result3.text)
+    result3 = trans.translate(korStr, dest="zh-cn")
+
+    print(f"입력하신 [{korStr}]는 영어로 [{result1.text}]입니다")
+    print(f"입력하신 [{korStr}]는 일본어로 [{result2.text}]입니다")
+    print(f"입력하신 [{korStr}]는 중국어로 [{result3.text}]입니다")
